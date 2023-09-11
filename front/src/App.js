293e814449614309f9039c29dbdafcb34ea10e3e
Jihye An <koanko.dev@gmail.com>
2023-09-11 13:36:58 +0900

    feat: create react app and connect with fastAPI

---
+++ front/src/App.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [msg, setMsg] = useState(null);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/hello").then((response) => {
+      response.json().then((json) => {
+        setMsg(json.message);
+      });
+    });
+  }, []);
+
+  return <div>{msg !== null ? msg : "loading..."}</div>;
+}
+
+export default App;
